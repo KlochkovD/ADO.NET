e1c7d7130e074fd10a995f0e0f1ae2a7b709f679
nb <disha07@mail.ru>
2021-08-01 14:45:46 +0300

    -

--- Linq_Student/Program.cs
+++ Linq_Student/Program.cs
@@ -11,36 +11,110 @@ namespace Linq_Student
 
         static void Main(string[] args)
         {
-
             IEnumerable<Student> studentQuery =
-            from student in students
-            where student.Scores[0] > 90
+            from student in Student.students
+            where student.Scores[0] > 90 && student.Scores[3] < 80
+            orderby student.Scores[0] descending
             select student;
 
-            foreach (Student student in studentQuery)
+            var studentQuery2 =
+            from student in Student.students
+            group student by student.Last[0];
+
+            var studentQuery3 =
+            from student in Student.students
+            group student by student.Last[0];
+
+            var studentQuery4 =
+            from student in Student.students
+            group student by student.Last[0] into studentGroup
+            orderby studentGroup.Key
+            select studentGroup;
+
+            var studentQuery5 =
+            from student in Student.students
+            let totalScore = student.Scores[0] + student.Scores[1] +
+            student.Scores[2] + student.Scores[3]
+            where totalScore / 4 < student.Scores[0]
+            select student.Last + " " + student.First;
+
+            var studentQuery6 =
+            from student in Student.students
+            let totalScore = student.Scores[0] + student.Scores[1] +
+            student.Scores[2] + student.Scores[3]
+            select totalScore;
+
+            double averageScore = studentQuery6.Average();
+            Console.WriteLine("Class average score = {0}", averageScore);
+
+            IEnumerable<string> studentQuery7 =
+                from student in Student.students
+                where student.Last == "Garcia"
+                select student.First;
+
+            var studentQuery8 =
+            from student in Student.students
+            let x = student.Scores[0] + student.Scores[1] +
+            student.Scores[2] + student.Scores[3]
+            where x > averageScore
+            select new { id = student.ID, score = x };
+
+            foreach (var item in studentQuery8)
             {
-                Console.WriteLine("{0}, {1}", student.Last, student.First);
+                Console.WriteLine("Student ID: {0}, Score: {1}", item.id, item.score);
             }
 
 
+            Console.WriteLine("The Garcias in the class are:");
+            foreach (string s in studentQuery7)
+            {
+                Console.WriteLine(s);
+            }
+
+
+            foreach (string s in studentQuery5)
+            {
+                Console.WriteLine(s);
+            }
+
+
+            foreach (var groupOfStudents in studentQuery4)
+            {
+                Console.WriteLine(groupOfStudents.Key);
+                foreach (var student in groupOfStudents)
+                {
+                    Console.WriteLine("   {0}, {1}",
+                        student.Last, student.First);
+                }
+            }
+
+
+            foreach (var groupOfStudents in studentQuery3)
+            {
+                Console.WriteLine(groupOfStudents.Key);
+                foreach (var student in groupOfStudents)
+                {
+                    Console.WriteLine("   {0}, {1}",
+                        student.Last, student.First);
+                }
+            }
+
+
+            foreach (var studentGroup in studentQuery2)
+            {
+                Console.WriteLine(studentGroup.Key);
+                foreach (Student student in studentGroup)
+                {
+                    Console.WriteLine("   {0}, {1}",
+                              student.Last, student.First);
+                }
+            }
+
+
+            
 
         }
-        public class Student
-        {
-            public string First { get; set; }
-            public string Last { get; set; }
-            public int ID { get; set; }
-            public List<int> Scores;
-        }
-
-        static List<Student> students = new List<Student>
-        {
-   new Student {First="Svetlana", Last="Omelchenko", ID=111, Scores= new List<int> {97, 92, 81, 60}},
-   new Student {First="Claire", Last="O’Donnell", ID=112, Scores= new List<int> {75, 84, 91, 39}},
-        };
-
     }
-
 
 }
 
